5aa2df641881e96ce2fecd4ddd821d89b3b14844
Dario de souza <sh4kaa@outlook.com>
2024-12-30 13:19:21 -0300

    chore: starting server node

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-const dados = 'bla bla'
+import express from 'express'
+
+const app = express()
+const PORT = 4500;
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`App is running in port ${PORT}`))
