d66a9243c753fe5c409d113239f56c6a6f572228
Dario de souza <sh4kaa@outlook.com>
2025-01-05 20:28:00 -0300

    refactor: refatoração

--- src/repositories/api/api.gemini.process.image.ts
+++ src/repositories/api/api.gemini.process.image.ts
@@ -1,11 +1,16 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
-import { MeterType } from "../../types/meter.type";
+import { MeterResponseIA, MeterType } from "../../types/meter.type";
+import { generateTemporarylink } from '../../utils/generate.temporary.link';
 
 
 export const apiGeminiProcessImage = async (payload: MeterType) => {
   const { image } = payload
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+  const apiKey = process.env.GEMINI_API_KEY as string
+  const genAI = new GoogleGenerativeAI(apiKey);
   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+  const prompt = 'Preciso que faça a medição ou leitura dos dados desse painel referente a quantidade de gás ou água usada tudo depende de qual tipo de painel contem na imagem. Me traga o resultado em com o texto em português Brasil'
+
   const result = await model.generateContent([
     {
       inlineData: {
@@ -13,8 +18,17 @@ export const apiGeminiProcessImage = async (payload: MeterType) => {
         mimeType: "image/jpeg",
       },
     },
-    'Preciso que faça a medição ou leitura dos dados desse painel referente a quantidade de gás ou água usada tudo depende de qual tipo de painel contem na imagem. Me traga o resultado em com o texto em português Brasil',
+    prompt,
   ]);
-  return result.response.text()
+  const match = result.response.text().match(/\d+/)
+  const meter = match ? +match[0] : 0
+  const id = String(result.response?.candidates?.[0].avgLogprobs!)
+  const uri = await generateTemporarylink(image, apiKey)
 
+  const responseGeminiApi: MeterResponseIA = {
+    image_url: uri,
+    measure_uuid: id,
+    measure_value: meter
+  }
+  return responseGeminiApi
 }
