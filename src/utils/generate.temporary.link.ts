efe81151b3462ac5d03c569d1b01e6c3759d3f0a
Dario de souza <sh4kaa@outlook.com>
2025-01-11 17:02:40 -0300

    chore: teste upload imagem para gemini api com sucesso

--- src/utils/generate.temporary.link.ts
+++ src/utils/generate.temporary.link.ts
@@ -2,6 +2,7 @@ import { GoogleAIFileManager } from "@google/generative-ai/server";
 import { join } from "path";
 import { tmpdir } from "os";
 import { writeFileSync } from "fs";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export const generateTemporarylink = async (img: Base64URLString, apiKey: string) => {
   const fileManager = new GoogleAIFileManager(apiKey);
@@ -9,7 +10,7 @@ export const generateTemporarylink = async (img: Base64URLString, apiKey: string
   const tempFilePath = join(tmpdir(), 'temp_image.jpg')
   const imageBuffer = Buffer.from(img, 'base64')
   writeFileSync(tempFilePath, imageBuffer)
-
+  console.log(tempFilePath)
   const uploadResult = await fileManager.uploadFile(
     tempFilePath,
     {
@@ -17,5 +18,18 @@ export const generateTemporarylink = async (img: Base64URLString, apiKey: string
       displayName: "upload image temporary",
     },
   );
+
+  const genAI = new GoogleGenerativeAI(apiKey);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const result = await model.generateContent([
+    "Descreva essa imagem",
+    {
+      fileData: {
+        fileUri: uploadResult.file.uri,
+        mimeType: uploadResult.file.mimeType,
+      },
+    },
+  ]);
+  console.log('resultado URI: ', result.response.text(), uploadResult.file.uri);
   return uploadResult.file.uri
 }
